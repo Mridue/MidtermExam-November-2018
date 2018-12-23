befdc5cee274bf5139bbc169a2e7db3c1b7ac580
Mridue <arif.mridue@gmail.com>
2018-12-23 18:14:37 -0500

    "UseMap problem done"

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,7 +12,34 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		Queue<Integer> newQ = new LinkedList<Integer>();
+
+		newQ.add(1);
+		newQ.add(2);
+		newQ.add(3);
+		newQ.add(4);
+		newQ.add(5);
+
+		System.out.println(newQ.peek());
+		System.out.println(newQ.poll());
+		System.out.println(newQ.peek());
+		System.out.println(newQ.remove());
+		System.out.println(newQ.peek());
+
+
+
+
+		for (Integer in : newQ) {
+			System.out.println(in);
+		}
+
+
+
+		Iterator itr = newQ.iterator();
+		while (itr.hasNext()) {
+			Integer i = (Integer) itr.next();
+			System.out.println(i);
 
 	}
 
-}
+}}
